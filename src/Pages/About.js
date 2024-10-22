6cf64642ffca7c9e13d0f5f63c6f33580b9d6c58
Godwin Chinkwe <chinkwegodwin@gmail.com>
2024-10-22 17:37:04 +0100

    slight

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -3,7 +3,7 @@ import "./CSS/About.css"
 import bout from "../Components/Assets/bout us.jpg"
 import mission from "../Components/Assets/ouur mission.jpg"
 // import {useNavigate} from "react-router-dom"
-import { NavLink } from "react-router-dom";
+// import { NavLink } from "react-router-dom";
 
 const About = () => {
   // const navigate = useNavigate();
@@ -18,7 +18,7 @@ const About = () => {
           <div className='about-hero-content'>
             <h2> Welcome to Our website</h2>
             <p>At Adrian Perfect Framing Limited, we take pride in creating distinctive and superior picture frames that cater to your precise requirements. Our team of passionate craftsmen, combined with our state-of-the-art facilities, ensures that every product meets our high standards of quality and excellence. </p>
-            <NavLink to="Contact/"> <button className='about-cta-button'> Contact us</button> </NavLink>
+            {/* <NavLink to="Contact/"> <button className='about-cta-button'> Contact us</button> </NavLink> */}
             {/* <button className='about-cta-button'> Contact us</button> */}
           </div>
           <div className='about-hero-image'>
@@ -31,14 +31,12 @@ const About = () => {
       <section className='about_us'>
         <div className='about-heading'>
           <h1>Our Mission</h1>
-
         </div>
         <div className='about-containerxy'>
-        <div className='about-hero-image'>
-          <div className='about-hero-content'>
-          <img src={mission} alt='' />
-            
-          </div>
+          <div className='about-hero-image'>
+            <div className='about-hero-content'>
+             <img src={mission} alt='' />
+            </div>
           </div>
           <div className='about-hero-content'>
             <h2> Our Vision, Our Passion.</h2>
@@ -46,12 +44,8 @@ const About = () => {
             </p>
             <button className='about-cta-button'> Contact us</button>
           </div>
-         
         </div>
-
       </section> 
-    
-      
     </div>
   )
 }
